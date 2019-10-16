26e87b92140710c9caf4525860ead73b2c25b866
Bartlomiej Grabowski <bartlomiej.grabowski@globallogic.com>
2019-10-16 14:59:10 +0200

    Initial commit for Woman In Technology example

---
+++ CodeCracker.Test/CodeCrackerTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace CodeCrackerTests
+{
+    [TestFixture]
+    public class CodeCrackerTest
+    {
+        //a b c d e f g h i j k l m n o p q r s t u v w x y z
+        // ! ) " ( £ * % & > < @ a b c d e f g h i j k l m n o
+
+        [Test]
+        public static void ShouldEncrypt_AToExclamation()
+        {
+            Assert.True(true);
+        }
+    }
+
+
+    public class CodeCracker
+    {
+        public static string Encrypt(string s)
+        {
+            return null;
+        }
+    }
+}
